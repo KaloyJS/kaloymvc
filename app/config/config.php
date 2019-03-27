a6481af9fa5eef8da83c40cd66ce02805564a267
Kaloy <kaloywebdev@gmail.com>
2019-03-26 22:03:33 -0400

    Completed base framework.

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+	// DB params
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '123456');
+	define('DB_NAME', 'kaloymvc');
+
+	// App Root
+	define('APPROOT', dirname(dirname(__FILE__)));
+	// URL Root
+	define('URLROOT', 'http://localhost:100/kaloymvc2');
+	// Site Name
+	define('SITENAME', 'KaloyMVC');
