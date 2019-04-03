f010bc5dc9ee1e2ba185bf956075b25fc4b52838
Kaloy <kaloywebdev@gmail.com>
2019-04-02 21:44:58 -0400

    Completed KaloyMVC.

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,13 @@
 <?php
 	// DB params
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '123456');
-	define('DB_NAME', 'kaloymvc');
+	define('DB_USER', '_YOUR_USER_');
+	define('DB_PASS', '_YOUR_PASS_');
+	define('DB_NAME', '_YOUR_DBNAME_');
 
 	// App Root
 	define('APPROOT', dirname(dirname(__FILE__)));
 	// URL Root
-	define('URLROOT', 'http://localhost:100/kaloymvc2');
+	define('URLROOT', '_YOUR_URL_');
 	// Site Name
-	define('SITENAME', 'KaloyMVC');
+	define('SITENAME', '_YOUR_SITENAME_');
